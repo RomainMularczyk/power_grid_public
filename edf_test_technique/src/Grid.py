354437bf16e6bf3aa1dd7ec21b46cc6d00782ecf
Romain Mularczyk <romain.mularczyk@gmail.com>
2023-06-11 00:44:04 +0200

    [MOD] classes representing grid entities in entities/ folder

--- edf_test_technique/src/Grid.py
+++ edf_test_technique/src/Grid.py
@@ -1,6 +1,6 @@
-from edf_test_technique.src.EnergyStorageSystem import EnergyStorageSystem
-from edf_test_technique.src.PhotovoltaicPowerPlant import PhotovoltaicPowerPlant
-from edf_test_technique.src.PointOfConnection import PointOfConnection
+from edf_test_technique.src.entities.EnergyStorageSystem import EnergyStorageSystem
+from edf_test_technique.src.entities.PhotovoltaicPowerPlant import PhotovoltaicPowerPlant
+from edf_test_technique.src.entities.PointOfConnection import PointOfConnection
 
 
 class Grid:
